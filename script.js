1668ed99b131f318fd70bcd236f54e403d40155e
Kacper Seredyn <kacper17@outlook.com>
2018-10-24 21:22:14 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -932,6 +932,103 @@ styleRenderers.refraction = function(canvas, isPreview) {
 
 	img.src = path;
 }
+styleRenderers.blinds = function(canvas, isPreview) {
+	var ctx = canvas.getContext("2d");
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+	var redURL, greenURL;	
+	
+	var rPath = imgPath("1_red" + (isPreview ? "_preview" : ""));
+	var rImg = new Image();
+
+	rImg.onload = function() {
+		ctx.drawImage(this, 0, 0, canvas.width, canvas.height);
+
+		ctx.fillStyle = prop("color1");
+		ctx.globalCompositeOperation = "multiply";
+		ctx.fillRect(0, 0, canvas.width, canvas.height);
+		
+		redURL = canvas.toDataURL();
+
+		var gPath = imgPath("1_green" + (isPreview ? "_preview" : ""));
+		var gImg = new Image();
+
+		gImg.onload = function() {
+			ctx.globalCompositeOperation = "source-over";
+			ctx.drawImage(this, 0, 0, canvas.width, canvas.height);
+			
+			ctx.fillStyle = prop("color2");
+			ctx.globalCompositeOperation = "multiply";
+			ctx.fillRect(0, 0, canvas.width, canvas.height);
+			
+			greenURL = canvas.toDataURL();
+			
+			bPath = imgPath("1_blue" + (isPreview ? "_preview" : ""));
+			var bImg = new Image();
+
+			bImg.onload = function() {
+				ctx.globalCompositeOperation = "source-over";
+				ctx.drawImage(this, 0, 0, canvas.width, canvas.height);
+				
+				ctx.fillStyle = prop("color3");
+				ctx.globalCompositeOperation = "multiply";
+				ctx.fillRect(0, 0, canvas.width, canvas.height);
+				
+				ctx.globalCompositeOperation = "screen";
+				
+				var r_Img = new Image();
+				r_Img.onload = function() {
+					ctx.drawImage(this, 0, 0, canvas.width, canvas.height);
+					
+					var g_Img = new Image();
+					g_Img.onload = function() {
+						ctx.drawImage(this, 0, 0, canvas.width, canvas.height);
+						ctx.globalCompositeOperation = "source-over";
+						finishExporting(isPreview);
+					}
+					g_Img.src = greenURL;
+				}
+				r_Img.src = redURL;
+			}
+			bImg.src = bPath;
+		}
+		gImg.src = gPath;
+	}
+
+	rImg.src = rPath;
+}
+styleRenderers.single_construct = function(canvas, isPreview) {
+	var ctx = canvas.getContext("2d");
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	
+	var path = imgPath("full" + (isPreview ? "_preview" : ""));
+	var img = new Image();
+
+	img.onload = function() {
+		ctx.drawImage(this, 0, 0, canvas.width, canvas.height);
+
+		ctx.globalCompositeOperation = "source-over";
+		finishExporting(isPreview);
+	}
+
+	img.src = path;
+}
+styleRenderers.single_arg = function(canvas, isPreview) {
+	var ctx = canvas.getContext("2d");
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	
+	var path = imgPath("arg" + prop("idx") + (isPreview ? "_preview" : ""));
+	var img = new Image();
+
+	img.onload = function() {
+		ctx.drawImage(this, 0, 0, canvas.width, canvas.height);
+
+		ctx.globalCompositeOperation = "source-over";
+		finishExporting(isPreview);
+	}
+
+	img.src = path;
+}
 
 function prop(name) {
 	return document.querySelector("article[data-style="+currentStyle+"] *[data-prop="+name+"]").value;
